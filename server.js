78ec2e53f22a42ab2d7461bb2c6f1ffd9d96a6d5
Ravikumar Thota <suneetha.gopu3@gmailcom>
2024-01-18 10:54:05 -0500

    created utilities

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const router = express.Router();
+const usersCtrl = require('./controllers/api/users');
 
 const port = process.env.PORT || 3001;
 const app = express();
+const ensureLoggedIn = require('./config/ensureLoggedIn');
 
 // --------------middleware----------
 
@@ -17,8 +19,15 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(require('./config/checkToken'));
 
+// --------------Routes----------
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
+app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
 
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // --------------Server----------
 
