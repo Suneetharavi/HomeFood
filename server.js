1e04a3654012d376a7cf6479bd6be695dacceb0f
Ravikumar Thota <suneetha.gopu3@gmailcom>
2024-01-17 19:50:23 -0500

    Connected to Express

---
+++ server.js
@@ -0,0 +1,27 @@
+// --------------Requires----------
+require('dotenv').config();
+//require('./config/database')
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const router = express.Router();
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+// --------------middleware----------
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+//app.use(require('./config/checkToken'));
+
+
+
+
+// --------------Server----------
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
+  });
