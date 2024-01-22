27269c9dd36be653bd35fcfeef4727571893d23d
Ravikumar Thota <suneetha.gopu3@gmailcom>
2024-01-22 10:52:22 -0500

    AboutUs Page cchanged

--- src/components/ConatctUs/ContactUs.jsx
+++ src/components/ConatctUs/ContactUs.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './ContactUs.css'
+import Footer from '../Footer/Footer'
 
 function ContactUs() {
   return (
+    <>
     <div className='contactClass'>
         <div className='fley1Class'><img className='FleyImg'src='AlluduKitchenFlyer1.jpeg' alt='flyer'/></div>
         <div><img className='FleyImg'src ='AlluduKitchenFlyer.jpeg' alt='flyer2'/></div>
     </div>
+    <Footer/>
+    </>
   )
 }
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,87 +2,65 @@
 import { Box, Typography } from "@mui/material";
 import styles from './Register.css'
 
-const Register=() =>{
-   
+const Register = () => {
+
   return (
     <>
-    <Box
-        sx={{
-          my: 4,
-          textAlign: "center",
-          p: 2,
-          "& h4": {
-            fontWeight: "bold",
-            my: 2,
-            fontSize: "2rem",
-            color:'purple',
-            
-          },
-          "& p": {
-            textAlign: "justify",
-          },
-          "@media (max-width:500px)": {
-            mt: 0,
-            "& h4 ": {
-              fontSize: "1.5rem",
-            },
-          },
-        }}
-      ></Box>
-    <div className='RegisterMainDiv'>
-        
-    <img className='AlluduLogoRegister' src='LogoAlluduKitchen.png' width='100px' alt='NoImg'></img>
-        <Typography variant="h4" color='green'>Welcome To Alludu's Kitchen</Typography><br/>
+      <div className='RegisterMainDiv'>
+
+        <img className='AlluduLogoRegister' src='LogoAlluduKitchen.png' width='100px' alt='NoImg'></img>
+        <Typography variant="h4" color='green'>Welcome to Alludu's Kitchen</Typography><br />
         <p>
-          Hi, I am Suneetha. An ambitious and Technology Driven Software Engineer 
-                who is 
-          Able to code HTML, Javascript, React, CSS from scratch on a daily basis. 
-          Recently completed 450+ hours of hands-on technical instruction in software development, 
+          Hi, I am Suneetha. An ambitious and Technology Driven Software Engineer
+          who is
+          Able to code HTML, Javascript, React, CSS from scratch on a daily basis.
+          Recently completed 450+ hours of hands-on technical instruction in software development,
           aiming to leverage my abilities to successfully fill the Software engineer role on the team.
-          </p><p>
-          An individual contributor who adds value through technical expertise and creates a clear strategy and delivers results through teamwork. 
-          </p>
-          
-        <br /><br/>
-        <p>
-        
-        ●   	Developed object-oriented web applications using Java programming language.
-        ●   	Designed user interfaces for laboratory systems that were intuitive and easy-to-use.
-        ●   	Maintained records of program development activities and results.
-        ●   	Worked successfully with a diverse group of coworkers to accomplish goals and address issues   related to our Institution.
-        ●   	Demonstrated leadership by making improvements to work processes and helping to train others.
-        ●   	Maintained schedule of class assignments to meet deadlines.
-        ●   	Helped maintain positive relations between the students and faculty.
-        ●   	Troubleshooting office equipment, computer hardware and software issues.
-        ●   Processed and prepared business or government forms and expense reports.
-        ●   Tracked students' attendance, materials distribution and assignments submitted in Excel.
+        </p><p>
+          An individual contributor who adds value through technical expertise and creates a clear strategy and delivers results through teamwork.
         </p>
-        <br/>
+
+        <br /><br />
         <p>
-        "I place great value on interpersonal relationships and a positive work environment at work. 
-        I like being recognized for my hard work and dedication, and offer positive feedback to others 
-        when I notice great work being done.
-         I value mutual respect and validation.
+
+          ●   	Developed object-oriented web applications using Java programming language.
+          ●   	Designed user interfaces for laboratory systems that were intuitive and easy-to-use.
+          ●   	Maintained records of program development activities and results.
+          ●   	Worked successfully with a diverse group of coworkers to accomplish goals and address issues   related to our Institution.
+          ●   	Demonstrated leadership by making improvements to work processes and helping to train others.
+          ●   	Maintained schedule of class assignments to meet deadlines.
+          ●   	Helped maintain positive relations between the students and faculty.
+          ●   	Troubleshooting office equipment, computer hardware and software issues.
+          ●   Processed and prepared business or government forms and expense reports.
+          ●   Tracked students' attendance, materials distribution and assignments submitted in Excel.
         </p>
-        <br/>
+        <br />
         <p>
-        I wish to become a Principal Software Engineer in the coming future. As I have acquired my higher education in technology,
-         I have built a strong base in different levels of programming.
-        </p><br/>
+          "I place great value on interpersonal relationships and a positive work environment at work.
+          I like being recognized for my hard work and dedication, and offer positive feedback to others
+          when I notice great work being done.
+          I value mutual respect and validation.
+        </p>
 
-      <p>Yes. My passion towards designing and developing websites makes me a right person to take this rigorous hand-on training and my dedication, smartwork , critical thinking and problem solving analysis skills makes me a right person for this position.
-        My desire is to be a world class Software Developer who renders solutions for various problems, with building applications,
-      being a great people manager and a great mentor to upcoming developers in the technology industry.</p>
-      <br></br>
-      <p>My Ability to code HTML, Javascript, React, CSS make me confident in achieving my goal. </p>
-      My  Recent experience in  450+ hours of hands-on technical instruction in software development,aiming to leverage my abilities to successfully fill the Software engineer role on the team.
-<br/><br/>
-      My Ambition to become a Software Engineer ,my dedication towards course learning , my confidence in coding with React, HTML, CSS technologies showcase the reality in achieving my Goal.
+        <p>
+          I wish to become a Principal Software Engineer in the coming future. As I have acquired my higher education in technology,
+          I have built a strong base in different levels of programming.
+        </p><br />
 
-      
-    </div> 
-    <Box/>
+        <p>Yes. My passion towards designing and developing websites makes me a right person to take this rigorous hand-on training and my dedication, smartwork , critical thinking and problem solving analysis skills makes me a right person for this position.
+          My desire is to be a world class Software Developer who renders solutions for various problems, with building applications,
+          being a great people manager and a great mentor to upcoming developers in the technology industry.</p>
+
+        <p>My Ability to code HTML, Javascript, React, CSS make me confident in achieving my goal. </p>
+        My  Recent experience in  450+ hours of hands-on technical instruction in software development,aiming to leverage my abilities to successfully fill the Software engineer role on the team.
+        <br /><br />
+        My Ambition to become a Software Engineer ,my dedication towards course learning , my confidence in coding with React, HTML, CSS technologies showcase the reality in achieving my Goal.
+        <br />
+
+      </div>
+      {/* <Box/> */}
     </>
-)}
-  
+  )
+}
+
 export default Register
