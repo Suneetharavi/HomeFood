8766cf65a1445d7858476e7e6c77ed160d39323e
Ravikumar Thota <suneetha.gopu3@gmailcom>
2024-01-22 19:09:17 -0500

    MenuCard page updated

--- src/components/ConatctUs/ContactUs.jsx
+++ src/components/ConatctUs/ContactUs.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './ContactUs.css'
 import Footer from '../Footer/Footer'
+import Navbar from '../MyNavbar'
 
 function ContactUs() {
   return (
     <>
+    <Navbar/>
     <div className='contactClass'>
         <div className='fley1Class'><img className='FleyImg'src='AlluduKitchenFlyer1.jpeg' alt='flyer'/></div>
         <div><img className='FleyImg'src ='AlluduKitchenFlyer.jpeg' alt='flyer2'/></div>
